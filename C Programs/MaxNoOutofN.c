1a1c9298e1f8892c5fd7947b372919ce6d64a16f
GunjanSharma02 <kmgunjan25@gmail.com>
2020-05-09 01:15:12 +0530

    Commiting the code of Maximum number out of N.

---
+++ C Programs/MaxNoOutofN.c
@@ -0,0 +1,31 @@
+//@Print the max number out of 5 numbers ?
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int  maxNumber, totalNumber;
+    int findMaxNumber(int numbers[], int totalNumber);
+    printf("Enter the count of numbers.\n");
+    scanf("%d",&totalNumber);
+    printf("Please Enter N Numbers.\n");
+
+    int numbers[totalNumber];
+    for(int i=0 ; i<totalNumber; i++){
+        scanf("%d",&numbers[i]);
+    }
+    maxNumber= findMaxNumber(numbers,totalNumber);
+    printf("Maximum Number is %d ",maxNumber);
+    //return 0;
+}
+
+int findMaxNumber(int numbers[], int totalNumber){
+    int maxNumber=0;
+    for(int i=0 ; i<totalNumber; i++){
+
+        if(maxNumber < numbers[i]){
+           maxNumber = numbers[i];
+        }
+    }
+    return maxNumber;
+
+}
